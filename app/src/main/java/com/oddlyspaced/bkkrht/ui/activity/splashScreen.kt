40e1258e84d8e44ea5e0565f6eec4e7a4aa7e555
bhawna <bhawnabc2002@gmail.com>
2022-09-12 13:18:21 +0530

    added the task

--- app/src/main/java/com/oddlyspaced/bkkrht/ui/activity/splashScreen.kt
+++ app/src/main/java/com/oddlyspaced/bkkrht/ui/activity/splashScreen.kt
@@ -1,11 +1,27 @@
 package com.oddlyspaced.bkkrht.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import com.oddlyspaced.bkkrht.R
 
 class splashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
+        //this one is for hiding taskBar
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+        //backgroundImg.startAnimation(topAnim)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
     }
 }
